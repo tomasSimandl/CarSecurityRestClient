9eb9a0f48bee490b4fbea384067367fd9765f632
Tomáš Šimandl <tomas.simandl@gmail.com>
2019-01-21 21:58:12 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/tomas/carsecurity/CarsecurityApplication.kt
@@ -0,0 +1,12 @@
+package com.example.tomas.carsecurity
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CarsecurityApplication
+
+fun main(args: Array<String>) {
+	runApplication<CarsecurityApplication>(*args)
+}
+
