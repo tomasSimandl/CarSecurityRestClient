e5e8d2be4c7608fd94b157f8b05e75c97a0b2690
Tomáš Šimandl <tomas.simandl@gmail.com>
2019-01-23 10:27:55 +0100

    Added findAllByCarId query to RouteRepository

--- src/main/kotlin/com/example/tomas/carsecurity/repository/RouteRepository.kt
+++ src/main/kotlin/com/example/tomas/carsecurity/repository/RouteRepository.kt
@@ -1,6 +1,13 @@
 package com.example.tomas.carsecurity.repository
 
 import com.example.tomas.carsecurity.model.Route
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.PagingAndSortingRepository
 
-interface RouteRepository : CrudRepository<Route, Long>
+interface RouteRepository : PagingAndSortingRepository<Route, Long> {
+
+    @Query("SELECT * FROM route r WHERE r.car_id = ?1", nativeQuery = true)
+    fun findAllByCarId(carId: Long, pageable: Pageable): Page<Route>
+}
