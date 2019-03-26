9db17714f9cac59fd3eacfac33298fa6486cc45c
Tomáš Šimandl <tomas.simandl@gmail.com>
2019-03-26 19:53:49 +0100

    Removed unused tests

--- src/test/kotlin/com/example/tomas/carsecurity/controller/MappingKtTest.kt
+++ src/test/kotlin/com/example/tomas/carsecurity/controller/MappingKtTest.kt
@@ -16,34 +16,4 @@ class MappingKtTest {
         val result = createJsonSingle("", "")
         assertEquals("{\"\":\"\"}", result)
     }
-
-    @Test
-    fun `add params empty params`(){
-
-        val map = HashMap<String, String>()
-
-        val result = addParams("url", map)
-        assertEquals("url", result)
-    }
-
-    @Test
-    fun `add params one param`(){
-
-        val map = HashMap<String, String>()
-        map["a"] = "10"
-
-        val result = addParams("url", map)
-        assertEquals("url?a=10", result)
-    }
-
-    @Test
-    fun `add params two params`(){
-
-        val map = HashMap<String, String>()
-        map["a"] = "10"
-        map["b"] = "20"
-
-        val result = addParams("url", map)
-        assertEquals("url?a=10&b=20", result)
-    }
 }
