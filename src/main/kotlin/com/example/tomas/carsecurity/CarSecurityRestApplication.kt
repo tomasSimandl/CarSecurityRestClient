7a6c6edcf9dc5621383e0c53ce4cdb2f9155b967
Tomáš Šimandl <tomas.simandl@gmail.com>
2019-04-08 15:09:12 +0200

    Load firebase configuration from properties

--- src/main/kotlin/com/example/tomas/carsecurity/CarSecurityRestApplication.kt
+++ src/main/kotlin/com/example/tomas/carsecurity/CarSecurityRestApplication.kt
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.core.io.ClassPathResource
@@ -15,6 +16,14 @@ class CarSecurityRestApplication
 /** Logger of this class. */
 private val logger = LoggerFactory.getLogger(CarSecurityRestApplication::class.java)
 
+/** URL of firebase database */
+@Value("\${firebase.database.url}")
+private val firebaseDatabaseUrl: String = ""
+
+/** Relative path from resources to json configuration file */
+@Value("\${firebase.configuration.file}")
+private val firebaseConfigFile: String = ""
+
 /**
  * Method run whole application and initialize service for communication with Firebase servers.
  */
@@ -22,10 +31,10 @@ fun main(args: Array<String>) {
     runApplication<CarSecurityRestApplication>(*args)
 
     try {
-        val serviceAccount = ClassPathResource("carsecurity-firebase.json").inputStream
+        val serviceAccount = ClassPathResource(firebaseConfigFile).inputStream
         val options = FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://carsecurity-72699.firebaseio.com")
+                .setDatabaseUrl(firebaseDatabaseUrl)
                 .build()
 
         FirebaseApp.initializeApp(options)
